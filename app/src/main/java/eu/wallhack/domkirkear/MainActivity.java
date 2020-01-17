f94cfbc8832753ed316b5832cfe829a44e679806
Haakon Meihack <haakon.meihack+github@gmail.com>
2020-01-17 21:22:34 +0100

    Ask for camera permission. (#6)
    * Ask for camera permission.
    * Mostly complete .travis.yml
    
    Co-authored-by: vegardwalland <43780927+vegardwalland@users.noreply.github.com>

--- app/src/main/java/eu/wallhack/domkirkear/MainActivity.java
+++ app/src/main/java/eu/wallhack/domkirkear/MainActivity.java
@@ -1,8 +1,11 @@
 package eu.wallhack.domkirkear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import eu.wallhack.domkirkear.common.PermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PermissionHelper.askCameraPermission(MainActivity.this);
+    }
+
+    // Callback for the permission request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PermissionHelper.CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted
+            } else {
+                // Permission was not granted
+                // TODO Quit app?
+            }
+        }
     }
 }
 
