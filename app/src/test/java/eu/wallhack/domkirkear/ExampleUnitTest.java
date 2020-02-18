410e6979733a30b3710ffb8aa73cfd3d3f006e6f
Vegard Walland <vegard.walland@gmail.com>
2020-02-18 16:00:48 +0100

    Add test for gps coordinates calculation

--- app/src/test/java/eu/wallhack/domkirkear/ExampleUnitTest.java
+++ app/src/test/java/eu/wallhack/domkirkear/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package eu.wallhack.domkirkear;
 
+import com.google.ar.sceneform.math.Vector3;
+
 import org.junit.Test;
+
+import eu.wallhack.domkirkear.common.LocationUtils;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +17,25 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testLocalGridCoordinates() {
+        Vector3 mainVector = new Vector3(58.938198f, 0f, 5.695740f);
+        Vector3 nodeVector = new Vector3(58.938329f, 0f, 5.695950f);
+
+        Vector3 localVector = LocationUtils.createLocalLocation(nodeVector, mainVector);
+        Double length = Math.sqrt(Math.pow(localVector.x, 2) + Math.pow(localVector.z, 2));
+
+        System.out.println("X: " + localVector.x + " Y: " + localVector.y + " Z: " + localVector.z + " Length: " + length);
+
+        assertBetween(length, 19.0, 20.0);
+        //assertBetween(localVector.x, 19f, 20f);
+        //assertBetween(localVector.z,);
+
+    }
+
+    private static void assertBetween(Double testValue, Double lowerParameter, Double higherParameter) {
+        assertTrue(testValue > lowerParameter && testValue < higherParameter);
     }
 }
