1822ea98338e2a694c66a4a5b9f4ce0adb8186a9
Haakon Meihack <haakon.meihack+github@gmail.com>
2020-02-24 15:59:27 +0100

    Ask for camera and location permissions

--- app/src/main/java/eu/wallhack/domkirkear/MainActivity.java
+++ app/src/main/java/eu/wallhack/domkirkear/MainActivity.java
@@ -1,14 +1,25 @@
 package eu.wallhack.domkirkear;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.wikitude.architect.ArchitectView;
+import com.wikitude.common.CallStatus;
+import com.wikitude.common.devicesupport.Feature;
+
+import java.util.EnumSet;
+
+import eu.wallhack.domkirkear.common.PermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private TextView debugText;
+    private CallStatus geoStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,37 @@ public class MainActivity extends AppCompatActivity {
         debugText = findViewById(R.id.topView);
         String test = "Hello, World\n" + TAG;
         debugText.setText(test);
+
+        EnumSet<Feature> neededFeatures = EnumSet.of(Feature.GEO);
+        geoStatus = ArchitectView.isDeviceSupporting(this, neededFeatures);
+
+        acquirePermissions();
     }
 
+    private void acquirePermissions() {
+        if (!PermissionHelper.getCameraPermission(this)) {
+            PermissionHelper.askCameraPermission(this);
+        }
+        if (!PermissionHelper.getLocationPermission(this)) {
+            PermissionHelper.askLocationPermission(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PermissionHelper.CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Granted
+                } else {
+                    // Not granted
+                }
+            case PermissionHelper.LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Granted
+                } else {
+                    // Not granted
+                }
+        }
+    }
 }
--- app/src/main/java/eu/wallhack/domkirkear/common/PermissionHelper.java
+++ app/src/main/java/eu/wallhack/domkirkear/common/PermissionHelper.java
@@ -12,7 +12,7 @@ public class PermissionHelper {
     // Ties the permission request to a corresponding permission request callback.
     // Value is arbitrary.
     public static final int CAMERA_REQUEST_CODE = 9001;
-    public static final int GPS_REQUEST_CODE = 9002;
+    public static final int LOCATION_REQUEST_CODE = 9002;
 
     public static void askCameraPermission(Activity activity) {
         if (!getCameraPermission(activity)) {
@@ -40,19 +40,19 @@ public class PermissionHelper {
         // TODO Implement localization
     }
 
-    // Permissions for GPS
-    public static void askGPSPermission(Activity activity) {
-        if (!getGPSPermission(activity)) {
-            askForGPSPermission(activity);
+    // Permissions for location
+    public static void askLocationPermission(Activity activity) {
+        if (!getLocationPermission(activity)) {
+            askForLocationPermission(activity);
         }
     }
 
-    public static boolean getGPSPermission(Activity activity) {
+    public static boolean getLocationPermission(Activity activity) {
         return ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
-    private static void askForGPSPermission(Activity activity){
-        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, GPS_REQUEST_CODE);
+    private static void askForLocationPermission(Activity activity){
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
     }
 
 }
